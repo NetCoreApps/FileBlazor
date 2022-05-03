e7eece136a2f2039fc792a3e0b2b732e87de6eed
Darren Reid <darren@reidmail.org>
2022-05-03 17:37:07 +1000

    Try fix paths.

--- FileBlazor/Configure.AppHost.cs
+++ FileBlazor/Configure.AppHost.cs
@@ -47,7 +47,7 @@ public class AppHost : AppHostBase, IHostingStartup
                 {
                     if (ctx.Dto is IAppFile { FileAccessType: { } } createFile)
                     {
-                        return $"/azure/{createFile.FileAccessType}/{ctx.FileName}";
+                        return ctx.GetLocationPath($"/azure/{createFile.FileAccessType}/{ctx.FileName}");
                     }
                     else
                         throw HttpError.BadRequest("Invalid file creation request.");
@@ -60,7 +60,7 @@ public class AppHost : AppHostBase, IHostingStartup
                 {
                     if (ctx.Dto is IAppFile { FileAccessType: { } } createFile)
                     {
-                        return $"/aws/{createFile.FileAccessType}/{ctx.FileName}";
+                        return ctx.GetLocationPath($"/aws/{createFile.FileAccessType}/{ctx.FileName}");
                     }
                     else
                         throw HttpError.BadRequest("Invalid file creation request.");
@@ -73,7 +73,7 @@ public class AppHost : AppHostBase, IHostingStartup
                 {
                     if (ctx.Dto is IAppFile { FileAccessType: { } } createFile)
                     {
-                        return $"/local/{createFile.FileAccessType}/{ctx.FileName}";
+                        return ctx.GetLocationPath($"/local/{createFile.FileAccessType}/{ctx.FileName}");
                     }
                     else
                         throw HttpError.BadRequest("Invalid file creation request.");
